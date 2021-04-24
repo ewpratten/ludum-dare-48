79cf3e727415287a7c7e887e4953f9471f8e3aa1
Evan Pratten <ewpratten@gmail.com>
2021-04-23 23:34:07 -0400

    Basic movement

--- src/player.rs
+++ src/player.rs
@@ -6,6 +6,7 @@ use raylib::math::Vector2;
 pub struct Player {
     pub position: Vector2,
     pub direction: Vector2,
+    pub size: Vector2,
     pub coins: u32,
     pub boost_percent: f32
 }
@@ -14,6 +15,10 @@ impl Player {
     pub fn new() -> Self {
         Self {
             boost_percent: 1.0,
+            size: Vector2 {
+                x: 11.0 * 4.0,
+                y: 21.0 * 4.0
+            },
             ..Default::default()
 			
         }
